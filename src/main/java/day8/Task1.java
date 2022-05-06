1b43baa0bd016026aaa447bce6ab228e6261f916
UseGR <deadmorozz50@gmail.com>
2022-05-06 12:46:13 +0300

    Add days 1 - 4

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,25 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        long startTime1 = System.currentTimeMillis();
+        String s1 = "";
+        for (int i = 0; i <= 20000; i++) {
+            s1 = s1 + " " + i;
+        }
+        System.out.print(s1);
+        long stopTime1 = System.currentTimeMillis();
+        System.out.println();
+        long startTime2 = System.currentTimeMillis();
+        StringBuilder sb = new StringBuilder("");
+        for (int i = 0; i <= 20000; i++) {
+            sb.append(i).append(" ");
+        }
+        System.out.println(sb);
+        long stopTime2 = System.currentTimeMillis();
+
+        System.out.println("--------------------------");
+        System.out.println("Время выполнения программы через конкатенацию строк: " + (stopTime1 - startTime1) + " мс");
+        System.out.println("Время выполнения программы через StringBuilder: " + (stopTime2 - startTime2) + " мс");
 
     }
 }
