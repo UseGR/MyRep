e365bf3860d4884ad7d5a10a131f3e854a84baf3
UseGR <deadmorozz50@gmail.com>
2022-05-13 23:54:34 +0300

    complete day15 task

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,35 @@
 package day15;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+    File input = new File("missingShoes.txt");
+        File output = new File("shoes.txt"); //пришлось создать файл shoes с расширением txt, а не csv, потомучто среда разработки почему то не читала из расширения csv
+        try {
+            PrintWriter pw = new PrintWriter(input);
+            Scanner scanner = new Scanner(output);
+            while(scanner.hasNextLine()) {
+                String s = scanner.nextLine();
+                String[] array = s.split(";");
 
+                if (array.length != 3)
+                    throw new Exception();
+
+
+                if (Integer.parseInt(array[2]) == 0)
+                    pw.println(s);
+
+            }
+            pw.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (Exception e) {
+            System.out.println("Некорректный входной файл");
+        }
     }
 }
+
